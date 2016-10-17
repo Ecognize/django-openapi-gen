8f7886039bb0c8a2ea5c5bedce9f51c929032539
Alex Revin <lyssdod@gmail.com>
2016-10-17 12:49:55 +0300

    Added stub command

---
+++ management/commands/makehandlers.py
@@ -0,0 +1,10 @@
+import os
+from django.conf import settings
+from django.core.management import BaseCommand
+
+class Command(BaseCommand):
+    help = "Generates stub controllers for endpoints specified in API scheme (both YAML and JSON are supported)"
+
+    def handle(self, *args, **options):
+        pass
+
