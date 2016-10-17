dfc7a53566343dd2fb8fc1d83724c3a235489916
Alex Revin <lyssdod@gmail.com>
2016-10-17 13:04:01 +0300

    Added default args

--- management/commands/makehandlers.py
+++ management/commands/makehandlers.py
@@ -5,6 +5,10 @@ from django.core.management import BaseCommand
 class Command(BaseCommand):
     help = "Generates stub controllers for endpoints specified in API scheme (both YAML and JSON are supported)"
 
+    def add_arguments(self, parser):
+        parser.add_argument('filename', required = True, help = 'API scheme file to process')
+        parser.add_argument('--dryrun', default = True, help = 'Only lists handlers which will be generated (enabled by default')
+
     def handle(self, *args, **options):
         pass
 
