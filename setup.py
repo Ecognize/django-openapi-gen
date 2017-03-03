59a2a269eb388a7acbd63c10b907d313108e8374
Alex Revin <lyssdod@gmail.com>
2017-03-03 17:56:23 +0200

    Added PyPI files

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+  name = 'djsw_wrapper',
+  packages = ['djsw_wrapper'],
+  version = '0.1.1',
+  description = 'Allows to build REST API directly from Swagger schema',
+  author = 'Alex Revin',
+  author_email = 'lyssdod@gmail.com',
+  url = 'https://github.com/lyssdod/django-openapi-gen',
+  download_url = 'https://github.com/lyssdod/django-openapi-gen/archive/0.1.1.tar.gz',
+  keywords = ['django', 'swagger', 'schema', 'django-rest-framework'],
+  classifiers = [],
+)
+
+
