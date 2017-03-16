2a043d0784130229de54a05c77cd9b18739daf5b
Alex Revin <lyssdod@gmail.com>
2017-03-16 15:21:42 +0200

    Fetch upstream requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
+import sys
+import contextlib
 from distutils.core import setup
+
+if (sys.version_info > (3, 0)):
+    from urllib.request import urlopen as urlopen
+else:
+    from urllib2 import urlopen
+
+txt = None
+req = 'https://raw.githubusercontent.com/ErintLabs/django-openapi-gen/master/requirements.txt'
+
+with contextlib.closing(urlopen(req)) as u:
+    txt = [x.decode('utf-8') for x in u.read().splitlines()]
 
 setup(
   name = 'djsw_wrapper',
@@ -9,7 +22,7 @@ setup(
   author_email = 'lyssdod@gmail.com',
   url = 'https://github.com/ErintLabs/django-openapi-gen',
   download_url = 'https://github.com/ErintLabs/django-openapi-gen/archive/0.1.5.tar.gz',
-  install_requires = ['django', 'djangorestframework', 'jinja2', 'flex'],
+  install_requires = txt,
   keywords = ['django', 'swagger', 'schema', 'django-rest-framework'],
   classifiers = [],
 )
